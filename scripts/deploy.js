ee94290be27e148d06049d457ed6fa5734d872a4
010620sun <103442936+010620sun@users.noreply.github.com>
2025-12-16 10:20:11 +0900

    chore: fix minor syntax errors

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,12 +9,12 @@ async function main() {
   const MyToken = await ethers.getContractFactory("MyToken");
   const Staking = await ethers.getContractFactory("Staking");
   const contract = await MyToken.deploy(1000000000000000000n);
-  const contract2 = await Staking.deploy(await contract.getAddress());
+  const contract2 = await Staking.deploy(contract.target);
 
   console.log("Deploy tx hash:", contract.deploymentTransaction().hash);
 
-  const address = await contract.target;
-  const address2 = await contract2.target;
+  const address = contract.target;
+  const address2 = contract2.target;
   console.log("MyToken Contract deployed at:", address);
   console.log("Staking Contract deployed at:", address2);
 }
