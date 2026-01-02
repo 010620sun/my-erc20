3b0f1d093d7b6d8c76a9ddcbcfaf5676d34b47ad
010620sun <103442936+010620sun@users.noreply.github.com>
2026-01-02 16:46:21 +0900

    feature: add TokenManager contract for efficient structure

---
+++ contracts/TokenManager.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./MyToken.sol";
+
+contract TokenManager is AccessControl{
+
+    MyToken public immutable token;
+
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    constructor(address tokenAddress,address multisig){
+        token = MyToken(tokenAddress);
+        _grantRole(DEFAULT_ADMIN_ROLE, multisig);
+    }
+    
+    function pause() external onlyRole(PAUSER_ROLE){
+        token.pause();
+    }
+
+    function unpause() external onlyRole(DEFAULT_ADMIN_ROLE){
+        token.unpause();
+    }
+
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE){
+        token.mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyRole(BURNER_ROLE){
+        token.burn(from, amount);
+    }
+
+    function airdrop(address[] calldata to, uint256 amount)external onlyRole(DEFAULT_ADMIN_ROLE){
+        token.airdrop(to, amount);
+    }
+}
