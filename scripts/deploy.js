d4b7a2a1674bbbcce75931ae081ae30d39973fdf
010620sun <103442936+010620sun@users.noreply.github.com>
2026-01-05 16:01:41 +0900

    feature: deploy tokenManager and set roles in deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,27 +3,44 @@ const { ethers } = require("hardhat");
 async function main() {
   
   const [deployer] = await ethers.getSigners();
+  const multisig = process.env.MULTISIG_ADDRESS;
+
   const DECIMALS = 18;
   const initialSupply = ethers.parseUnits("1000000", DECIMALS);
 
   console.log("Using account:", deployer.address);
 
   const MyToken = await ethers.getContractFactory("MyToken");
+  const TokenManager = await ethers.getContractFactory("TokenManager");
   const Staking = await ethers.getContractFactory("Staking");
 
+  // MyToken deployment
   const mytoken = await MyToken.deploy(initialSupply);
   await mytoken.waitForDeployment();
   
-  const staking = await Staking.deploy(mytoken.target);
+   // TokenManager deployment
+  const tokenmanager = await TokenManager.deploy(mytoken.target,multisig);
+  await tokenmanager.waitForDeployment();
+
+  // Staking deployment
+  const staking = await Staking.deploy(mytoken.target,tokenmanager.target);
   await staking.waitForDeployment();
 
-  const MINTER_ROLE = await mytoken.MINTER_ROLE();
-  await mytoken.grantRole(MINTER_ROLE,staking.target);
+  // loads role values
+  const DEFAULT_ADMIN_ROLE = await mytoken.DEFAULT_ADMIN_ROLE();
+
+  // let TokenManager be manager of MyToken
+  await mytoken.grantRole(DEFAULT_ADMIN_ROLE,tokenmanager.target);
+
+  // deprive token admin role of deployer
+  await mytoken.renounceRole(DEFAULT_ADMIN_ROLE, deployer);
 
   console.log("Deploy tx hash:", mytoken.deploymentTransaction().hash);
 
   console.log("MyToken Contract deployed at:", mytoken.target);
   console.log("Staking Contract deployed at:", staking.target);
+  console.log("TokenManager Contract deployed at:", tokenmanager.target);
+  
 }
 
 main().catch((err) => {
